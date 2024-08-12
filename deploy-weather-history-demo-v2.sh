2b498b5b2b78851c1a9d553740a8e4536f0e1f1b
VedPriya Mishra <vedpriya@VedPriyas-MacBook-Air.local>
2024-08-12 00:15:10 -0700

    fixeddeployscript

--- deploy-weather-history-demo-v2.sh
+++ deploy-weather-history-demo-v2.sh
@@ -42,15 +42,15 @@ echo "Creating Apigee config..."
 
 apigeecli datacollectors create --name "dc_weather_history_v2_location" --type STRING --org "$PROJECT" --token "$TOKEN" 
 
-apigeecli targetservers create --name "bigquery" --host "bigquery.googleapis.com" --port 443 --sslinfo true --org "$PROJECT" --token "$TOKEN" --env "$APIGEE_ENV"
+apigeecli targetservers create --name "bigquery" --host "bigquery.googleapis.com" --port 443  --tlsenforce true --org "$PROJECT" --token "$TOKEN" --env "$APIGEE_ENV"
 
 apigeecli kvms create --name "weather-history-v2-kvm" --env "$APIGEE_ENV" --org "$PROJECT" --token "$TOKEN"
 
 apigeecli kvms entries create --map "weather-history-v2-kvm" --key "geocode-apikey" --value "$GEOCODE_APIKEY" --env "$APIGEE_ENV" --org "$PROJECT" --token "$TOKEN"
 
-find ./src -type f -name "*.xml" -exec sed -i "s/\[PROJECT\]/$PROJECT/" {} +
+find ./src -type f -name "*.xml" -exec sed -i '' "s/\[PROJECT\]/$PROJECT/" {} +
 
-find ./portal/specs -type f -name "*.yaml" -exec sed -i "s/\[APIGEE_HOST\]/$APIGEE_HOST/" {} +
+find ./portal/specs -type f -name "*.yaml" -exec sed -i '' "s/\[APIGEE_HOST\]/$APIGEE_HOST/" {} +
 
 echo "Importing and Deploying Apigee weather-history-v2 proxy..."
 REV=$(apigeecli apis create bundle -f "src/proxies/weather-history-v2/apiproxy"  -n weather-history-v2 --org "$PROJECT" --token "$TOKEN" --disable-check | jq ."revision" -r)
@@ -58,9 +58,9 @@ REV=$(apigeecli apis create bundle -f "src/proxies/weather-history-v2/apiproxy" 
 apigeecli apis deploy --wait --name weather-history-v2 --ovr --rev "$REV" --org "$PROJECT" --env "$APIGEE_ENV" --token "$TOKEN" --sa "$BIGQUERY_SERVICE_ACCOUNT"
 
 echo "Creating API Products"
-apigeecli products create --name weather-history-v2-premium-product --displayname "Weather History v2 Premium" --opgrp ./src/config/weather-history-v2-product-ops.json --envs "$APIGEE_ENV" --approval auto --quota 20 --interval 1 --unit minute --org "$PROJECT" --token "$TOKEN"
+apigeecli products create --name weather-history-v2-premium-product --display-name "Weather History v2 Premium" --opgrp ./src/config/weather-history-v2-product-ops.json --envs "$APIGEE_ENV" --approval auto --quota 20 --interval 1 --unit minute --org "$PROJECT" --token "$TOKEN"
 
-apigeecli products create --name weather-history-v2-freemium-product --displayname "Weather History v2 Freemium" --opgrp ./src/config/weather-history-v2-product-ops.json --envs "$APIGEE_ENV" --approval auto --quota 10 --interval 1 --unit minute --org "$PROJECT" --token "$TOKEN"
+apigeecli products create --name weather-history-v2-freemium-product --display-name "Weather History v2 Freemium" --opgrp ./src/config/weather-history-v2-product-ops.json --envs "$APIGEE_ENV" --approval auto --quota 10 --interval 1 --unit minute --org "$PROJECT" --token "$TOKEN"
 
 echo "Creating Developer"
 apigeecli developers create --user testuser --email weather-history-v2-developer@acme.com --first A --last User --org "$PROJECT" --token "$TOKEN"
